403ed7eec576d1b9e037132f245656512ce44294
Nurliman Diara <nurlimandiara@gmail.com>
2024-01-22 11:45:42 +0700

    Add CommonJS module format to tsup.config.ts

--- packages/env-valibot/tsup.config.ts
+++ packages/env-valibot/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig((options) => {
   return {
     entry: ["src/index.ts", "src/core.ts", "src/nuxt.ts", "src/nextjs.ts"],
-    format: ["esm"],
+    format: ["esm", "cjs"],
     clean: true,
     dts: true,
     sourcemap: !options.watch,
