3c24d493900a7f72d4cc0aaaf345f20351cb9504
nurliman <nurlimandiara@gmail.com>
2024-05-09 06:06:30 +0700

    use bun shell instead of execa

---
+++ .changeset/quiet-llamas-buy.md
@@ -0,0 +1,5 @@
+---
+"@nurliman/env-valibot": patch
+---
+
+use bun shell instead of execa
--- scripts/publishDenoPackages.ts
+++ scripts/publishDenoPackages.ts
@@ -1,31 +1,35 @@
-import fs from "fs-extra";
+import fse from "fs-extra";
 import path from "node:path";
 import { getPackages } from "@manypkg/get-packages";
-import { execa } from "execa";
+import { $ } from "bun";
 
 async function publishDenoPackage(pkgPath = process.cwd()) {
   // get version from package.json
   const pkgJsonPath = path.join(pkgPath, "package.json");
 
   // check if package.json exists
-  if (!fs.existsSync(pkgJsonPath)) return;
+  if (!fse.existsSync(pkgJsonPath)) return;
 
   // write version to deno.json
   const denoJsonPath = path.join(pkgPath, "deno.json");
 
   // check if deno.json exists
-  if (!fs.existsSync(denoJsonPath)) return;
+  if (!fse.existsSync(denoJsonPath)) return;
 
-  await execa("npx", ["jsr", "publish", "--allow-dirty"], {
-    cwd: pkgPath,
-    stdio: "inherit",
-  });
+  const denoJson = await fse.readJson(denoJsonPath);
+
+  // check if version exists
+  if (!denoJson.version) return;
+
+  await $`bunx jsr publish --allow-dirty`;
 }
 
 async function main() {
   const { packages, rootPackage } = await getPackages(process.cwd());
   const allPackages = [rootPackage, ...packages];
-  const publishTasks = allPackages.map((pkg) => publishDenoPackage(pkg.dir));
+  const publishTasks = allPackages.map((pkg) => {
+    if (pkg?.dir) publishDenoPackage(pkg.dir);
+  });
   await Promise.all(publishTasks);
 }
 
