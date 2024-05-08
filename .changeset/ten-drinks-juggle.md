b621fb9afccf3c7de6f82aa36505dd10d71b6052
nurliman <nurlimandiara@gmail.com>
2024-05-08 19:42:11 +0700

    chore: Improve documentation in env-valibot module files

---
+++ .changeset/ten-drinks-juggle.md
@@ -0,0 +1,5 @@
+---
+"@nurliman/env-valibot": patch
+---
+
+Improve documentation in env-valibot module files
--- packages/env-valibot/src/index.ts
+++ packages/env-valibot/src/index.ts
@@ -1,5 +1,58 @@
 /**
- * index file for env-valibot. Contains exports for core, nextjs, nuxt, and presets.
+ * # Typesafe Envs made Simple
+ * 
+ * Deploying your app with invalid environment variables is a hassle. This package helps you to avoid that.
+ * 
+ * ## Usage
+ *
+ * > For full documentation, see https://env.t3.gg
+ *
+ * This package supports the full power of Valibot, meaning you can use `transforms` and `default` values.
+ *
+ * ### Define your schema
+ *
+ * ```ts
+ * // src/env.mjs
+ * import { createNextjsEnv } from "@nurliman/env-valibot";
+ * import * as v from "valibot";
+ *
+ * export const env = createNextjsEnv({
+ *   // Serverside Environment variables, not available on the client.
+ *   // Will throw if you access these variables on the client.
+ *   server: {
+ *     DATABASE_URL: v.string([v.url()]),
+ *     OPEN_AI_API_KEY: v.string([v.minLength(1)]),
+ *   },
+ *
+ *   // Environment variables available on the client (and server).
+ *   // 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+ *   client: {
+ *     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: v.string([v.minLength(1)]),
+ *   },
+ *
+ *   // Due to how Next.js bundles environment variables on Edge and Client,
+ *   // we need to manually destructure them to make sure all are included in bundle.
+ *   // 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+ *   runtimeEnv: {
+ *     DATABASE_URL: process.env.DATABASE_URL,
+ *     OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+ *     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+ *       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+ *   },
+ * });
+ * ```
+ *
+ * ### Use the schema in your app with autocompletion and type inference
+ *
+ * ```ts
+ * // src/app/hello/route.ts
+ * import { env } from "../env.mjs";
+ *
+ * export const GET = (req: Request) => {
+ *   const DATABASE_URL = env.DATABASE_URL;
+ *   // use it...
+ * };
+ * ```
  * @module
  */
 
